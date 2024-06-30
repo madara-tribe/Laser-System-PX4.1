86048f7e11bf9d819ebbbf4a29334039292b2408
madara-tribe <tsukuba.univer@gmail.com>
2024-06-30 19:54:54 +0900

    px4.1

---
+++ Hardware/hardware_thread.py
@@ -0,0 +1,55 @@
+import sys
+from multiprocessing import Queue
+import time
+import Adafruit_PCA9685
+
+def x_angle2duty(angle):
+    plus = True if angle > 0 else False
+    duty = int( float(angle) * 2.17 + 102 )
+    # reverse
+    # duty = 594 - duty
+    print('x reset angle is', f' {angle}' 'duty is' f' {duty}')
+    return duty if plus==True else -duty
+
+def y_angle2duty(angle):
+    plus = True if angle > 0 else False
+    duty = int(float(angle) * 2.17 + 102 )
+    print('y reset angle is', f' {angle}' 'duty is' f' {duty}')
+    return duty if plus==True else -duty
+
+
+def hw_thread(q:Queue, opt):
+    xaxis_pin = opt.xaxis_pin
+    yaxis_pin = opt.yaxis_pin
+    pwm = Adafruit_PCA9685.PCA9685()
+    # Set frequency to 60hz, good for servos.
+    pwm.set_pwm_freq(opt.pwm_freq)
+    x_defalut = x_angle2duty(angle=opt.x_defalut)
+    pwm.set_pwm(xaxis_pin, 0, x_defalut)
+    time.sleep(0.5)
+    y_defalut = y_angle2duty(angle=opt.y_defalut)
+    pwm.set_pwm(yaxis_pin, 0, y_defalut)
+    time.sleep(0.5)
+    try:
+        while True:
+            duty = q.get()
+            axis, angle = duty
+            if axis=='x':
+                duty = x_angle2duty(angle)
+                pwm.set_pwm(xaxis_pin, 0, duty)
+                #time.sleep(0.1)
+            elif axis=='y':
+                duty = y_angle2duty(angle)
+                pwm.set_pwm(yaxis_pin, 0, duty)
+                #time.sleep(0.1)
+    finally:
+        pwm.set_pwm(xaxis_pin, 0, x_defalut)
+        time.sleep(1)
+        pwm.set_pwm(yaxis_pin, 0, y_defalut)
+        time.sleep(1)
+        sys.exit()
+
+#if __name__ == "__main__":
+  #  q = Queue()
+  #  hw_thread(q, opt=None)
+    
--- main.py
+++ main.py
@@ -1,23 +1,71 @@
 import argparse
 import yaml
-from software.dual_camera import DualCamera
-        
+from tkinter import Tk
+from multiprocessing import Process, Queue
+
+
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dual', action='store_true', help='use 2 CSI cameras')
-    parser.add_argument('--plot', action='store_true', help='just plot and not TCP socket trasmission to pyside')
-    parser.add_argument('--hyp', type=str, default='csicam_utils/hyp.csi.imx219.yaml', help='CSI IMX219 hyperparameters path')
+    parser.add_argument('--pwm', action='store_true', help='real action')
+    # soft ware
+    parser.add_argument('-st', '--software_test', action='store_true', help='software test, inrefence and plot')
+    parser.add_argument('--frame_interval', type=int, default=5, help='camera interval to reduce burden')
+    parser.add_argument('--onnx_path', type=str, default='yolov7Tiny_640_640.onnx', help='image path')
+    parser.add_argument('--cpu', type=str, default='True', help='if cpu is None, use CUDA')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='conf threshold for NMS or postprocess')
+    parser.add_argument('--max_disparity', type=int, default=1000, help='max disparity')
+    parser.add_argument('--min_disparity', type=int, default=10, help='max disparity')
+    parser.add_argument('--hyp', type=str, default='setup_utils/csicam/hyp.csi.imx219.yaml', help='CSI IMX219 hyperparameters path')
+    parser.add_argument('--x_coord', type=int, default=1141, help='x default coordinate')
+    parser.add_argument('--y_coord', type=int, default=494, help='y default coordinate') 
+   # hard ware
+    parser.add_argument('-ht', '--hard_test', action='store_true', help='hardware calibrate')
+    parser.add_argument('--y_defalut', type=int, default=155, help='y default angle')
+    parser.add_argument('--x_defalut', type=int, default=57, help='x default angle')
+    parser.add_argument('--xaxis_pin', type=int, default=15, help='x pin number')
+    parser.add_argument('--yaxis_pin', type=int, default=14, help='y pin number')
+    parser.add_argument('--pwm_freq', type=int, default=60, help='pwm frequency')
     opt = parser.parse_args()
     return opt
  
     
-def main(opt, hyp):
-    DualCamera(opt, hyp)
-        
-if __name__ == '__main__':
+def software_threads(q):
+    from software.dual_camera import DualCamera
     opt = get_parser()
     # load hyps dict
     with open(opt.hyp, errors='ignore') as f:
         hyp = yaml.safe_load(f)
-    main(opt, hyp)
+    DualCamera(q, opt, hyp)
+
+def hardware_threads(q):
+    opt = get_parser()
+    if opt.hard_test:
+        from Hardware.tkinter_pan_tilt import App
+        root = Tk()
+        root.wm_title('Servo Control')
+        app = App(root, opt)
+        root.geometry("220x120+0+0")
+        root.mainloop()
+    elif opt.pwm:
+        from Hardware.hardware_thread import hw_thread 
+        hw_thread(q, opt)
+
+if __name__ == '__main__':
+    opt = get_parser()
+    q = Queue()
+    if opt.hard_test:
+        p2 = Process(target = hardware_threads, args=(q,))
+        p2.start()
+        p2.join()
+    elif opt.software_test:
+        p1 = Process(target = software_threads, args=(q,))
+        p1.start()
+        p1.join()
+    elif opt.pwm:
+        p1 = Process(target = software_threads, args=(q,))
+        p2 = Process(target = hardware_threads, args=(q,))
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
 
